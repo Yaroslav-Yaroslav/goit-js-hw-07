59d11925a82113cdac7a662a26c69f629419ba4a
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2023-02-11 14:13:27 +0200

    completed the task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const divRef = document.querySelector(".gallery");
+const imagesMarkup = makeGalleryImagesMarkup(galleryItems);
+divRef.insertAdjacentHTML("afterbegin", imagesMarkup);
+
+divRef.addEventListener("click", onImagePreviewClick);
+
+function makeGalleryImagesMarkup(galleryImages) {
+  return galleryImages
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onImagePreviewClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const imagePreview = event.target;
+  const instance = basicLightbox.create(
+    `<img src="${imagePreview.dataset.source}">`,
+    {
+      onClose: () => {
+        window.removeEventListener("keydown", onEscKeyDown);
+      },
+    }
+  );
+  instance.show();
+
+  window.addEventListener("keydown", onEscKeyDown);
+  function onEscKeyDown(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+    console.log(event);
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+listRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
-console.log(galleryItems);
+function createGalleryMarkup(galleryImages) {
+  return galleryImages
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join("");
+}
